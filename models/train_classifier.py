9cfe976b2c9318103e67a8f66b6bc2ee150a56a2
Jani <je260641834@gmail.com>
2019-02-17 16:22:38 +0100

    Uploading initital files

---
+++ models/train_classifier.py
@@ -0,0 +1,109 @@
+import sys
+import nltk
+import numpy as np
+nltk.download(['punkt', 'wordnet'])
+from nltk.tokenize import word_tokenize, RegexpTokenizer
+from nltk.stem import WordNetLemmatizer
+import pandas as pd
+from sqlalchemy import create_engine
+import re
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn.tree import DecisionTreeClassifier
+import pickle
+
+def load_data(database_filepath):
+    name = 'sqlite:///' + database_filepath
+    engine = create_engine(name)
+    df = pd.read_sql_table('Disasters', con=engine) # is table always called this? 
+    X = df['message']
+    y = df[df.columns[5:]]
+    added = pd.get_dummies(df[['related','genre']])
+    y = pd.concat([y, added], axis=1)
+    category_names = y.columns
+    return X, y, category_names
+
+def tokenize(text):
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+    
+    # take out all punctuation while tokenizing
+    tokenizer = RegexpTokenizer(r'\w+')
+    tokens = tokenizer.tokenize(text)
+    
+    # lemmatize as shown in the lesson
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+    return clean_tokens
+
+def build_model():
+    # Create pipeline with Classifier
+    moc = MultiOutputClassifier(RandomForestClassifier())
+
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', moc)
+        ])
+    
+    return pipeline
+    
+def evaluate_model(model, X_test, y_test, category_names):
+    # Get results and add them to a dataframe.
+    y_pred = model.predict(X_test)
+    results = pd.DataFrame(columns=['Category', 'f_score', 'precision', 'recall'])
+    num = 0
+    for cat in y_test.columns:
+        precision, recall, f_score, support = precision_recall_fscore_support(y_test[cat], y_pred[:,num], average='weighted')
+        results.set_value(num+1, 'Category', cat)
+        results.set_value(num+1, 'f_score', f_score)
+        results.set_value(num+1, 'precision', precision)
+        results.set_value(num+1, 'recall', recall)
+        num += 1
+    print('Aggregated f_score:', results['f_score'].mean())
+    print('Aggregated precision:', results['precision'].mean())
+    print('Aggregated recall:', results['recall'].mean())
+    return results
+
+def save_model(model, model_filepath):
+    pickle.dump(model, open(model_filepath, 'wb'))
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
