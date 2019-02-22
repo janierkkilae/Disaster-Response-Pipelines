9f72a5b8c6c308314c2a1b435928cc8ef3191ed3
Jani <je260641834@gmail.com>
2019-02-22 17:11:00 +0100

    Bug fix for data processing

--- data/process_data.py
+++ data/process_data.py
@@ -3,12 +3,29 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
+    """Load dataframe from filepaths
+
+    INPUT
+    messages_filepath -- str, link to file
+    categories_filepath -- str, link to file
+
+    OUTPUT
+    df - pandas DataFrame
+    """
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
     df = messages.merge(categories, on='id')
     return df
 
 def clean_data(df):
+    """Clean data included in the DataFrame and transform categories part
+
+    INPUT
+    df -- type pandas DataFrame
+
+    OUTPUT
+    df -- cleaned pandas DataFrame
+    """
     categories = df['categories'].str.split(pat=';', expand=True)
     row = categories.loc[0]
     colnames = []
@@ -23,16 +40,20 @@ def clean_data(df):
     df.drop('categories', axis=1, inplace=True)
     df = pd.concat([df, categories], axis=1)
     df.drop_duplicates(inplace=True)
+    # Removing entry that is non-binary
+    df = df[df['related'] != 2]
     print('Duplicates remaining:', df.duplicated().sum())
     return df
     
 def save_data(df, database_filename):
+    """Saves DataFrame (df) to database path"""
     name = 'sqlite:///' + database_filename
     engine = create_engine(name)
     df.to_sql('Disasters', engine, index=False)
 
 
 def main():
+    """Runs main functions: Loads the data, cleans it and saves it in a database"""
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
