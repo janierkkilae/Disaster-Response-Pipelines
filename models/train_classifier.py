2546ade80b9a3b4d542c5c7302e0832a2fe2a4ac
Jani <je260641834@gmail.com>
2019-02-22 17:06:07 +0100

    Updated train_classifier.py to use GridSearchCV

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -12,22 +12,23 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.multioutput import MultiOutputClassifier
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics import classification_report
 from sklearn.tree import DecisionTreeClassifier
 import pickle
 
 def load_data(database_filepath):
+    """Load the filepath and return the data"""
     name = 'sqlite:///' + database_filepath
     engine = create_engine(name)
     df = pd.read_sql_table('Disasters', con=engine) # is table always called this? 
+    print(df.head())
     X = df['message']
-    y = df[df.columns[5:]]
-    added = pd.get_dummies(df[['related','genre']])
-    y = pd.concat([y, added], axis=1)
+    y = df[df.columns[4:]]
     category_names = y.columns
     return X, y, category_names
 
 def tokenize(text):
+    """tokenize and transform input text. Return cleaned text"""
     url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
     detected_urls = re.findall(url_regex, text)
     for url in detected_urls:
@@ -46,7 +47,7 @@ def tokenize(text):
     return clean_tokens
 
 def build_model():
-    # Create pipeline with Classifier
+    """Return Grid Search model with pipeline and Classifier"""
     moc = MultiOutputClassifier(RandomForestClassifier())
 
     pipeline = Pipeline([
@@ -54,35 +55,41 @@ def build_model():
         ('tfidf', TfidfTransformer()),
         ('clf', moc)
         ])
-    
-    return pipeline
+
+    parameters = {'clf__estimator__max_depth': [10, 50, None],
+              'clf__estimator__min_samples_leaf':[2, 5, 10]}
+
+    cv = GridSearchCV(pipeline, parameters)
+    return cv
     
 def evaluate_model(model, X_test, y_test, category_names):
+    """Print model results
+
+    INPUT
+    model -- required, estimator-object
+    X_test -- required
+    y_test -- required
+    category_names = required, list of category strings
+
+    OUTPUT
+    None
+    """
     # Get results and add them to a dataframe.
     y_pred = model.predict(X_test)
+    print(classification_report(y_test, y_pred, target_names=category_names))
     results = pd.DataFrame(columns=['Category', 'f_score', 'precision', 'recall'])
-    num = 0
-    for cat in y_test.columns:
-        precision, recall, f_score, support = precision_recall_fscore_support(y_test[cat], y_pred[:,num], average='weighted')
-        results.set_value(num+1, 'Category', cat)
-        results.set_value(num+1, 'f_score', f_score)
-        results.set_value(num+1, 'precision', precision)
-        results.set_value(num+1, 'recall', recall)
-        num += 1
-    print('Aggregated f_score:', results['f_score'].mean())
-    print('Aggregated precision:', results['precision'].mean())
-    print('Aggregated recall:', results['recall'].mean())
-    return results
 
 def save_model(model, model_filepath):
+    """Save model as pickle file"""
     pickle.dump(model, open(model_filepath, 'wb'))
 
 def main():
+    """Load the data, run the model and save model"""
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25)
         
         print('Building model...')
         model = build_model()
